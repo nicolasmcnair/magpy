002bb74d95101bb39cb72c11167ab0830735eb8e
Nicolas McNair <nicolasmcnair@gmail.com>
2019-07-28 19:56:27 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='MagPy_TMS',
-    version='1.2.0b8',
+    version='1.2',
     description='A Python toolbox for controlling Magstim TMS stimulators via serial communication',
     long_description='A Python toolbox for controlling Magstim TMS stimulators via serial communication',
     author='Nicolas McNair',
     author_email='nicolas.mcnair@sydney.edu.au',
     url='http://github.com/nicolasmcnair/magpy',
-    classifiers=['Development Status :: 4 - Beta',
+    classifiers=['Development Status :: 5 - Production/Stable',
                  'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3'],
